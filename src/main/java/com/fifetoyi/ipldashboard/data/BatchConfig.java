d68eb37e872630d13b5ff2d93e4465923583341d
Fifetoyi <fifetoyi@gmail.com>
2021-05-08 12:22:08 +0100

    clean up code

--- src/main/java/com/fifetoyi/ipldashboard/data/BatchConfig.java
+++ src/main/java/com/fifetoyi/ipldashboard/data/BatchConfig.java
@@ -43,7 +43,8 @@ public class BatchConfig {
                     {
                         setTargetType(MatchInput.class);
                     }
-                }).build();
+                })
+                .build();
     }
 
     @Bean
@@ -57,7 +58,8 @@ public class BatchConfig {
                 .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
                 .sql("INSERT INTO match (id, city, date, player_of_match, venue, team1, team2, toss_winner, toss_decision, match_winner, result, result_margin, umpire1, umpire2) "
                         + " VALUES (:id, :city, :date, :playerOfMatch, :venue, :team1, :team2, :tossWinner, :tossDecision, :matchWinner, :result, :resultMargin, :umpire1, :umpire2)")
-                .dataSource(dataSource).build();
+                .dataSource(dataSource)
+                .build();
     }
 
     @Bean
