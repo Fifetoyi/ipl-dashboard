d25c08ade4041f3f9e009e6d9a4ed019207a36f8
Fifetoyi <fifetoyi@gmail.com>
2021-05-08 22:11:28 +0100

    create team repository

---
+++ src/main/java/com/fifetoyi/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.fifetoyi.ipldashboard.repository;
+
+import com.fifetoyi.ipldashboard.model.Team;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+    
+    Team findByTeamName(String teamName);
+    
+}
